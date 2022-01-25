68162f4a3213ba598325cace59bef365fb56f397
Chrescco <98253438+Chrescco@users.noreply.github.com>
2022-01-25 19:49:27 +0530

    Delete diagnostics.lua

--- diagnostics.lua
+++
@@ -1,6 +0,0 @@
-local signs = { Error = '✘', Warn = '', Hint = '', Info = 'ⁱ'}
-	for type, icon in pairs(signs) do
-		local hl = 'DiagnosticSign' .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-	end
-
