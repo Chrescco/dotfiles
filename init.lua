123ba2e2deadd54a1a5453c0f853065ecb0ef021
Chrescco <98253438+Chrescco@users.noreply.github.com>
2022-02-17 17:35:07 +0530

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,23 @@
+require('plugins')
+require('chrescco.bufferline')
+require('chrescco.lualine')
+require('chrescco.cmp')
+require('chrescco.lspconfig')
+require('chrescco.lspinstaller')
+require('chrescco.null ls')
+require('chrescco.nvimtree')
+require('chrescco.plenary')
+require('chrescco.treesitter')
+require('chrescco.autopairs')
+require('keybindings')
+require('diagnostics')
+require('config')
+require('chrescco.indent')
+
+vim.cmd ([[
+syntax on
+]])
+----------------------------|
+-- Options                  |
+----------------------------|
+
